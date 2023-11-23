3b804c9217612ef42b00411372e2649cfd8c7f04
oscar <echezuoscar@gmail.com>
2023-11-22 22:40:47 -0400

    Se coloco el navbar en fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,24 @@
-import ebenezerLogo from "../assets/ebenezerLogo.PNG" 
+import ebenezerLogo from "../assets/ebenezerLogo.PNG"
 import { PiMotorcycleFill } from "react-icons/pi"
 
 export default function Header() {
-    return (
-            <div className="wave-container flex flex-col h-102 w-full p-4 pb-12 lg:px-8">
-                <div>
-                    <img src={ebenezerLogo} alt="" className="w-44 h-16 lg:w-60 lg:h-16" />
-                </div>
-                <div className="flex flex-col w-full m-auto text-center items-center">
-                    <h2 className="font-extrabold text-transparent text-4xl bg-clip-text bg-gradient-to-r from-white via-white to-two lg:text-5xl">La forma más inteligente de comprar tu moto <span className="underline underline-offset-4 "></span></h2>
-                    <ul className="mt-8 text-2xl bg-two bg-opacity-70 p-2 cursor-pointer rounded-xl hover:bg-opacity-100">
-                        <a href="#form" className="flex gap-2 text-white">Quiero mi <span className="flex justify-center items-end mx-1"><PiMotorcycleFill/></span></a>
-                    </ul>
-                </div>
+  return (
+
+          <div className="">
+            <div className="fixed z-20 w-full">
+              <div className="bg-one flex justify-between py-4 px-4 md:py-4 md:px-14 ">
+                <img src={ebenezerLogo} alt="" className="w-40 h-12 lg:w-60 lg:h-16" />
+                <a href="#form" className="flex gap-2 justify-center items-center text-one text-xl p-2 bg-yellow-400 rounded-lg md:text-2xl">Quiero mi <span className="flex justify-center m-auto items-end mx-1"><PiMotorcycleFill/></span></a>
+              </div>
             </div>
-    )
+            <div className="wave-container flex flex-col w-full h-102 p-4 lg:px-8">
+              <div className="flex flex-col w-full m-auto text-center items-center">
+                <h2 className="text-whrite text-2xl lg:text-4xl mb-6">La forma más inteligente de comprar tu moto <span className="underline underline-offset-4 "></span></h2>
+              </div>
+            </div>
+          </div>
+
+
+
+  )
 }
